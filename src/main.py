3e4880f13f4f51aaa218b32bd65de3cb6fa8d1ea
GauthierDenglos <gauthier.denglos@epita.fr>
2021-09-29 21:02:11 +0200

    [ADD][Mute]

--- src/main.py
+++ src/main.py
@@ -16,37 +16,35 @@ async def on_ready():  # When the bot is ready
     print("I'm in")
     print(bot.user)  # Prints the bot's username and identifier
 
-@bot.event
-async def on_message(message):  # When !name is called
-    if message.content == "!name" :
-        response = message.author
-        await message.channel.send(response)
-
 @bot.command()
 async def pong(ctx):
     await ctx.send('pong')
 
 @bot.command()
 async def mute (ctx, member : discord.User=None, reason = None): # When !mute xxx is called / disabling all textual channels permissions
-    # server = ctx.message.server
-    # perms = discord.Permissions(send_messages=False, read_messages=True)
-    # role = get(ctx.guild.roles, name="Ghost")
+    perms = discord.Permissions(send_messages=False, read_messages=True)
+    role = ctx.get(ctx.guild.roles, name="Ghost")
     print("test 0")
-    # if role : 
-    #     print("test 1")
-    #     if Ghost in member.roles: 
-    #         await member.delete_roles(member, get(member.server.roles, name="Ghost"))
-    #     else:
-    #         await member.add_roles(member, get(member.server.roles, name="Ghost"))
-    # else :
-    #     print("test 2")
-    #     await ctx.create_role(server, name='Ghost', permissions=perms)
-    #     await member.add_roles(member, get(member.server.roles, name="Ghost"))
+    if role : 
+        print("test 1")
+        if Ghost in member.roles: 
+            await member.delete_roles(member, get(member.guild.roles, name="Ghost"))
+        else:
+            await member.add_roles(member, get(member.guild.roles, name="Ghost"))
+    else :
+        print("test 2")
+        await ctx.create_role(ctx.guild, name='Ghost', permissions=perms)
+        await member.add_roles(member, get(member.guild.roles, name="Ghost"))
 
 @bot.command()
 async def ban (ctx, member : discord.User=None, reason = None):
     await ctx.guild.ban(member, reason=reason)
     await ctx.channel.send(f"{member} is banned!")
+
+@bot.command()
+async def name (ctx):  # When !name is called
+    response = ctx.message.author
+    await ctx.message.channel.send(response)
 
 token = DISCORD_TOKEN
 bot.run(token)  # Starts the bot
