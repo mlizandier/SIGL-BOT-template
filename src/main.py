3a996d25c7806ba2733a3e115a6a955f9934c347
GauthierDenglos <gauthier.denglos@epita.fr>
2021-09-29 21:13:50 +0200

    [ADD][Mute] 2

--- src/main.py
+++ src/main.py
@@ -21,28 +21,6 @@ async def pong(ctx):
     await ctx.send('pong')
 
 @bot.command()
-async def mute (ctx, member : discord.User=None, reason = None): # When !mute xxx is called / disabling all textual channels permissions
-    perms = discord.Permissions(send_messages=False, read_messages=True)
-    role = ctx.get(ctx.guild.roles, name="Ghost")
-    print("test 0")
-    if role : 
-        print("test 1")
-        if Ghost in member.roles: 
-            await member.delete_roles(member, get(member.guild.roles, name="Ghost"))
-        else:
-            await member.add_roles(member, get(member.guild.roles, name="Ghost"))
-    else :
-        print("test 2")
-        await ctx.create_role(ctx.guild, name='Ghost', permissions=perms)
-        await member.add_roles(member, get(member.guild.roles, name="Ghost"))
-
-
-@bot.command()
-async def ban (ctx, member : discord.User=None, reason = None):
-    await ctx.guild.ban(member, reason=reason)
-    await ctx.channel.send(f"{member} is banned!")
-
-@bot.command()
 async def name (ctx):  # When !name is called
     response = ctx.message.author
     await ctx.message.channel.send(response)
@@ -56,6 +34,34 @@ async def admin (ctx, member : discord.User=None):
             admin_role = role
     await member.add_roles(admin_role)
     await ctx.channel.send(f'{member} is now an admin')
+
+@bot.command(name="mute")
+async def mute (ctx, member : discord.User=None): # When !mute xxx is called / disabling all textual channels permissions
+    ghost_role = None
+    for role in ctx.guild.roles:
+        if role.name == "GHOST":
+            ghost_role = role
+        else : 
+            perms = 
+            ctx.guild.create_role(name="GHOST", permissions=perms)
+    await member.add_roles(ghost_role)
+    await ctx.channel.send(f'{member} is now a ghost')
+    # print("test 0")
+    # if role : 
+    #     print("test 1")
+    #     if Ghost in member.roles: 
+    #         await member.delete_roles(member, get(member.guild.roles, name="Ghost"))
+    #     else:
+    #         await member.add_roles(member, get(member.guild.roles, name="Ghost"))
+    # else :
+    #     print("test 2")
+    #     await ctx.create_role(ctx.guild, name='Ghost', permissions=perms)
+    #     await member.add_roles(member, get(member.guild.roles, name="Ghost"))
+
+@bot.command(name="ban")
+async def ban (ctx, member : discord.User=None, reason = None):
+    await ctx.guild.ban(member, reason=reason)
+    await ctx.channel.send(f"{member} is banned!")
 
 token = DISCORD_TOKEN
 bot.run(token)  # Starts the bot
